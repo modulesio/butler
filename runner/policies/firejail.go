7194666a3ad54404e0a9cee23ea2a5781f93e187
Adrian Biedrzycki <a@modules.io>
2018-02-27 04:01:27 -0800

    Rewrite runner firejail policy

--- runner/policies/firejail.go
+++ runner/policies/firejail.go
@@ -7,7 +7,30 @@ package policies
 // whitelist doesn't seem to work with exclusions, though?
 
 const FirejailTemplate = `
-blacklist ~/.config/chromium
-blacklist ~/.config/chrome
-blacklist ~/.mozilla
+caps.drop all
+# ipc-namespace
+netfilter
+# no3d
+# nodvd
+# nogroups
+nonewprivs
+noroot
+# nosound
+# notv
+# novideo
+protocol unix,inet,inet6
+seccomp
+# shell none
+
+# disable-mnt
+# private
+# private-bin program
+# private-dev
+# private-etc none
+# private-lib
+# private-tmp
+
+# memory-deny-write-execute
+# noexec ${HOME}
+# noexec /tmp
 `
