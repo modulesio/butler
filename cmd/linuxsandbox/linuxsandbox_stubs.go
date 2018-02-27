acefa8f74e08bfd58e8d3e529ae53258471e7b87
Avaer Kazmer <a@modules.io>
2018-02-27 02:44:54 -0500

    Bugfix linuxsandbox stub on non-linux builds

--- cmd/linuxsandbox/linuxsandbox_stubs.go
+++ cmd/linuxsandbox/linuxsandbox_stubs.go
@@ -2,9 +2,16 @@
 
 package linuxsandbox
 
-import "github.com/modulesio/butler/mansion"
+import (
+  "github.com/modulesio/butler/mansion"
+  "github.com/itchio/wharf/state"
+)
 
 func Register(ctx *mansion.Context) {
 	// don't register anything
 	return
+}
+
+func Check(consumer *state.Consumer) error {
+  return nil
 }
