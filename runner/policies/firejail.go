6e5b7acf7df8fba3bbb9669d0deaa82f26f4e76a
Avaer Kazmer <a@modules.io>
2018-02-27 03:42:26 -0500

    Clean up firejail policy

--- runner/policies/firejail.go
+++ runner/policies/firejail.go
@@ -7,18 +7,6 @@ package policies
 // whitelist doesn't seem to work with exclusions, though?
 
 const FirejailTemplate = `
-blacklist ~/.config/itch/users
-blacklist ~/.config/itch/butler_creds
-blacklist ~/.config/itch/marketdb
-blacklist ~/.config/itch/Cookies
-blacklist ~/.config/itch/Partitions
-
-blacklist ~/.config/kitch/users
-blacklist ~/.config/kitch/butler_creds
-blacklist ~/.config/kitch/marketdb
-blacklist ~/.config/kitch/Cookies
-blacklist ~/.config/kitch/Partitions
-
 blacklist ~/.config/chromium
 blacklist ~/.config/chrome
 blacklist ~/.mozilla
